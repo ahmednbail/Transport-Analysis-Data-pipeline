0aa3dc62d37f2705209ab1e3c1b063f67399d102
ahmednbail <116032681+ahmednbail@users.noreply.github.com>
2026-05-14 03:22:04 +0300

    feat: initialize Airflow DAG for yellow taxi pipeline and update project requirements

--- dags/Main-DAG.py
+++ dags/Main-DAG.py
@@ -1,13 +1,27 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from upload import run as upload
+from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
+POSTGRES_CONN_ID= 'PostgreSQL_connection'
+GCS_BUCKET= '######'# bucket name فقط
+GCS_PATH = "#####"          # folder داخل الباكت
+GCS_FILENAME = "######"
+BIGQUERY_DATASET = "#######"
+BIGQUERY_TABLE = "#######"
+
+
 
 with DAG(
     dag_id='pipeline_for_yellow_taxi',
     start_date=datetime(2026,5,14),
-    schedule='@monthly',
-    catchup=False
+    schedule='@daily',
+    catchup=False,
+    default_args={
+        'retries':1,
+        'retry_delay':timedelta(minutes=1),
+    },
+    tags=['Data-Pipeline', 'Yellow-Taxi']
 ) as dag:
     
    pass
