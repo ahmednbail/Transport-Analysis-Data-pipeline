ab4149e235598b3dcf1c28dade5bc466a286c8c3
Ahmed Nabil <ahmednab14420@gmail.com>
2026-05-18 22:13:14 +0000

    finish

--- models/marts/dim_vendors.sql
+++ models/marts/dim_vendors.sql
@@ -1,1 +1,10 @@
-select 1
+with trips as(
+    select * from {{ref('stg_taxi')}}
+),
+vendors as (
+    select 
+      distinct vendor_id,
+      {{ get_vendor_data('vendor_id') }} as vendor_name
+    from trips
+)
+select * from vendors
