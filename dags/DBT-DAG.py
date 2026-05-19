8c14cde2e1c05d00dc254057f46c454cb0091871
ahmednbail <116032681+ahmednbail@users.noreply.github.com>
2026-05-19 04:18:43 +0300

    feat: add Airflow DAG to orchestrate dbt Cloud jobs with connection verification

--- dags/DBT-DAG.py
+++ dags/DBT-DAG.py
@@ -1,28 +1,43 @@
 from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.decorators import task
 from airflow.providers.dbt.cloud.operators.dbt import DbtCloudRunJobOperator
 
 
 DBT_CLOUD_CONN_ID = 'dbt-cloud'
-DBT_CLOUD_JOB_ID = 70506183131891 
+DBT_CLOUD_JOB_ID = 70506183131891
 
 
 with DAG(
-    dag_id= 'dbt-dag',
-    start_date= datetime(2026,5,16),
-    schedule= '@daily',
-    catchup= False,
+    dag_id='dbt-dag',
+    start_date=datetime(2026, 5, 16),
+    schedule='@daily',
+    catchup=False,
     default_args={
         'owner': 'Ahmed Nabil',
-        'retries': 3,
+        'retries': 2,
         'retry_delay': timedelta(minutes=1),
         'depends_on_past': False,
     },
-    tags= ['dbt', 'data-pipeline']
+    tags=['dbt', 'data-pipeline'],
+) as dag:
 
-) as dag: 
-           dbt_run=DbtCloudRunJobOperator(
-               task_id='dbt-run',
-               conn_id=DBT_CLOUD_CONN_ID,
-               job_id=DBT_CLOUD_JOB_ID,
-           )
+    @task
+    def check_dbt_connection():
+        from airflow.providers.dbt.cloud.hooks.dbt import DbtCloudHook
+
+        hook = DbtCloudHook(dbt_cloud_conn_id=DBT_CLOUD_CONN_ID)
+        response = hook._run_and_get_response(endpoint="")
+        data = response.json()
+        if data["status"]["is_success"]:
+            print(f"Connected to dbt Cloud account: {data['data']['name']}")
+        else:
+            raise Exception(f"Connection failed: {data['status']['user_message']}")
+
+    dbt_run = DbtCloudRunJobOperator(
+        task_id='dbt-run',
+        conn_id=DBT_CLOUD_CONN_ID,
+        job_id=DBT_CLOUD_JOB_ID,
+    )
+
+    check_dbt_connection() >> dbt_run
